ff132b7c490ef21ef4f7222a28692c059240fd1c
Joao Correia <joao@hedy.tech>
2023-04-12 07:41:35 +0200

    clean chat collection

--- contexts/ChatContextProvider.tsx
+++ contexts/ChatContextProvider.tsx
@@ -14,8 +14,7 @@ import {
 import { createContext, useLayoutEffect, useState } from 'react'
 import { firestore } from '../configs/firebase'
 
-// const COLLECTION_NAME = 'JoaoHedyYazenHealthTestChat'
-const COLLECTION_NAME = 'JoaoHedyTestChat'
+const COLLECTION_NAME = 'JoaoHedyYazenHealthTestChat'
 const RECORDS_PER_FETCH = 25
 export interface MessageContentText {
     type: 'text'
